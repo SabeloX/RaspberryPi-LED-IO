b1152799e92c6bb5ccb205a1e5c848ed25ac8342
SabeloX <sabelomtw@gmail.com>
2020-09-22 23:28:13 +0100

    Created a main method for lighting an LED

--- pushbutton-led.py
+++ pushbutton-led.py
@@ -1,4 +1,6 @@
-import RPi.HPIO as GPIO
+# Module imports
+import RPi.GPIO as GPIO
+import time
 
 # Using the GPIO ports
 GPIO.setmode(GPIO.BCM)
@@ -13,3 +15,17 @@ button_pin = 27
 # Setup pins
 GPIO.setup(led_pin, GPIO.OUT)
 GPIO.setup(button_pin, GPIO.IN)
+
+# Main function
+def main():
+	GPIO.output(led_pin, GPIO.HIGH)
+	print("The LED should be on.")
+	time.sleep(1) #1 second delay
+
+	GPIO.output(led_pin, GPIO.LOW)
+	print("The LED should be off.")
+
+# Execute the functions
+if __name__ == "__main__":
+	main()
+	GPIO.cleanup() #cleaup used channels
