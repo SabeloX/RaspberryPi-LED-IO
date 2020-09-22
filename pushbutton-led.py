16061257d002fa3eb0ffedb826face1e7dd6466e
SabeloX <sabelomtw@gmail.com>
2020-09-22 23:03:43 +0100

    Board pin setup as input and/or output

--- pushbutton-led.py
+++ pushbutton-led.py
@@ -1,1 +1,15 @@
 import RPi.HPIO as GPIO
+
+# Using the GPIO ports
+GPIO.setmode(GPIO.BCM)
+
+# Remove warning messages
+GPIO.setwarnings(False)
+
+# Pin variables
+led_pin = 17
+button_pin = 27
+
+# Setup pins
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(button_pin, GPIO.IN)
