ff684a97ff6d152de8f36851dc6423ef2c86efee
SabeloX <sabelomtw@gmail.com>
2020-09-23 18:35:54 +0100

    Added a method for push button and created a README.md file

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>LED and Pushbutton on RPi</h1>
+
+The <code>pushbutton-led.py</code> file contains two methods:
+
+- <code>led()</code> to toggle an LED once, connected to GPIO pin 18 on the Raspberry Pi
+- <code>pushbutton()</code> to use a push button connected to GPIO pin 17 to light an LED.
+	- Manually end the program after use.
--- pushbutton-led.py
+++ pushbutton-led.py
@@ -1,3 +1,4 @@
+
 # Module imports
 import RPi.GPIO as GPIO
 import time
@@ -9,15 +10,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Pin variables
-led_pin = 17
-button_pin = 27
+led_pin = 18
+button_pin = 17
 
 # Setup pins
 GPIO.setup(led_pin, GPIO.OUT)
-GPIO.setup(button_pin, GPIO.IN)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #give the input a default value, i.e 0
 
 # Main function
-def main():
+def led():
 	GPIO.output(led_pin, GPIO.HIGH)
 	print("The LED should be on.")
 	time.sleep(1) #1 second delay
@@ -25,7 +26,20 @@ def main():
 	GPIO.output(led_pin, GPIO.LOW)
 	print("The LED should be off.")
 
+	GPIO.cleanup()
+
+# Light LED using the switch
+def pushbutton():
+	try:
+		GPIO.add_event_detect(button_pin, GPIO.BOTH)
+		while True:
+			if(GPIO.event_detected(button_pin)):
+				GPIO.output(led_pin, not GPIO.input(button_pin))
+			time.sleep(0.1)
+	finally:
+		GPIO.output(led_pin, GPIO.LOW)
+		GPIO.cleanup()
+
 # Execute the functions
 if __name__ == "__main__":
-	main()
-	GPIO.cleanup() #cleaup used channels
+	pushbutton()
